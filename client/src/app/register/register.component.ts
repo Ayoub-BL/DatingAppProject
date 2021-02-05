0c163a25531f52cf4444013a900de41c433fb76b
Ayoub-BL <boulahya.ayoub@gmail.com>
2021-02-05 02:14:51 +0100

    Client login and register-5-2

--- client/src/app/register/register.component.ts
+++ client/src/app/register/register.component.ts
@@ -1,4 +1,6 @@
 import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { error } from 'protractor';
+import { AccountService } from '../_services/account.service';
 
 @Component({
   selector: 'app-register',
@@ -6,21 +8,24 @@ import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
   styleUrls: ['./register.component.css']
 })
 export class RegisterComponent implements OnInit {
-  @Input() usersFromHomeComponent: any;
   @Output() cancelRegister = new EventEmitter();
   model: any = {};
 
-  constructor() { }
+  constructor(private accountService: AccountService) { }
 
   ngOnInit(): void {
   }
 
   register() {
-    console.log(this.model);
+    this.accountService.register(this.model).subscribe(
+      response => {
+        console.log(response);
+        this.cancel();
+      }, error => console.log(error)
+    );
   }
 
   cancel() {
-    console.log('cancelled');
     this.cancelRegister.emit(false);
   }
 }
